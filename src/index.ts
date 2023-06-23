b42d8972fae666fe559d971dc6a076555d3739b2
garrcastro <garrcastro@gmail.com>
2023-06-23 13:06:16 -0300

    atualizado com os try/catch e tratamento de erros. Sem os extras.

--- src/index.ts
+++ src/index.ts
@@ -12,60 +12,136 @@ app.listen(3003, ()=>{
     console.log('Servidor rodando na porta 3003.')
 });
 
-//Endpoint de teste 'ping pong'
-app.get('/ping',(req: Request, res: Response) =>{
-    res.status(200).send('Pong!')
-})
 
 //Endpoint de 'Get All Users'
 app.get('/users',(req: Request, res: Response) => {
-    res.status(200).send(users)
+    try {
+        res.status(200).send(users)
+        
+    } catch (error) {
+        res.status(500).send("Algum erro inusitado aconteceu.")
+    }
 })
 
 //Endpoint de 'Get All Products/Search Products'
-/* app.get('/products', (req: Request, res: Response) => {
-    res.status(200).send(products)
-}) */
+
 app.get('/products', (req: Request, res: Response) => {
-    const name = req.query.name as string
-    if(name){
-        const productsByName = products.filter((product)=>{
-            return product.name.toLowerCase().includes(name.toLowerCase())
-        })
-        res.status(200).send(productsByName)
-    }else{
-        res.status(200).send(products)
+    try {
+        const name = req.query.name as string
+        if(name.length<1){
+            res.status(400)
+            throw new Error ("O nome do produto deve ter mais que uma letra.")
+        }
+        if(name){
+            const productsByName = products.filter((product)=>{
+                return product.name.toLowerCase().includes(name.toLowerCase())
+            })
+            res.status(200).send(productsByName)
+        }else{
+            res.status(200).send(products)
+        }
+        
+    } catch (error) {
+        res.status(500).send("Algum erro inusitado aconteceu.")
     }
 })
 
 //Endpoint de 'Create User'
 app.post('/users', (req: Request, res: Response) => {
-    const {id, name, email, password, createdAt} = req.body
-    const newUser:TUsers = {
-        id,
-        name,
-        email,
-        password,
-        createdAt
+    try {
+        const {id, name, email, password, createdAt} = req.body
+        const newUser:TUsers = {
+            id,
+            name,
+            email,
+            password,
+            createdAt
+        }
+        if (newUser.id!==undefined){
+            if(newUser.id && newUser.id[0]!=="u"){
+                res.status(400);
+                throw new Error('"ID" inválido. Deve iniciar com a letra "u"');
+            }
+            /* if (newUser.id===){
+                res.status(400)
+                throw new Error ("Já existe um usuário com este ID.")
+            } */
+        }
+        if (newUser.name!==undefined){
+            if (typeof(newUser.name)!=="string"){
+                res.status(400)
+                throw new Error ("O nome de usuário deve ser composto apenas por letras.")
+            }
+        }
+        if (newUser.email!==undefined){
+            if (typeof(newUser.email)!=="string"){
+                res.status(400)
+                throw new Error ("O email deve ser string.")
+            }
+            /* if (newUser.email===){
+                res.status(400)
+                throw new Error ("Este email já está cadastrado.")
+            } */
+        }
+        if (newUser.password!==undefined){
+            if (typeof(newUser.password)!=="string"){
+                res.status(400)
+                throw new Error ("A senha deve ser composto apenas por letras.")
+            }
+        }
+        
+        users.push(newUser)
+        res.status(201).send('Usuário cadastrado com sucesso!')
+        console.table(users)
+        
+    } catch (error) {
+        res.status(500).send("Algum erro inusitado aconteceu.")
     }
-    users.push(newUser)
-    res.status(201).send('Usuário cadastrado com sucesso!')
-    console.table(users)
 })
 
 //Endpoint de 'Create Product'
 app.post('/products', (req: Request, res: Response) => {
-    const {id, name, price, description, imageUrl} = req.body
-    const newProduct:TProducts = {
-        id,
-        name,
-        price,
-        description,
-        imageUrl
+    try {
+        const {id, name, price, description, imageUrl} = req.body
+        const newProduct:TProducts = {
+            id,
+            name,
+            price,
+            description,
+            imageUrl
+        }
+        if (newProduct.id!==undefined){
+            if(newProduct.id && newProduct.id[0]!=="p" && newProduct.id[1]!=="r" && newProduct.id[2]!=="o" && newProduct.id[3]!=="d"){
+                res.status(400);
+                throw new Error('"ID" inválido. Deve iniciar com o termo "prod"');
+            }
+            /* if (newProduct.id===){
+                res.status(400)
+                throw new Error ("Já existe um produto com este ID.")
+            } */
+        }
+        if (newProduct.name!==undefined){
+            if (typeof(newProduct.name)!=="string"){
+                res.status(400)
+                throw new Error ("O nome do produto deve ser composto apenas por letras.")
+            }
+        }
+        if (newProduct.price!==undefined){
+            if (typeof(newProduct.price)!=="number"){
+                res.status(422)
+                throw new Error ("O valor de Price deve ser numérico.")
+            }
+            if(newProduct.price<0){
+                res.status(400)
+                throw new Error ("O valor de Price deve ser maior que zero.")
+            }
+        }
+        products.push(newProduct)
+        res.status(201).send('Produto cadastrado com sucesso!')
+        console.table(products)
+    } catch (error) {
+        res.status(500).send("Algum erro inusitado aconteceu.")
     }
-    products.push(newProduct)
-    res.status(201).send('Produto cadastrado com sucesso!')
-    console.table(products)
 })
 
 //Endpoint de 'Delete User by Id'
@@ -145,21 +221,10 @@ app.put('/products/:id', (req: Request, res: Response) => {
             product.price = newPrice>0 ? newPrice : product.price
             product.description = newDescription || product.description
             product.imageUrl = newImageUrl || product.imageUrl
-            
-            
-            //account.balance = isNaN(newBalance) ? account.balance : newBalance
-            
         }
     
         res.status(200).send("Produto atualizado com sucesso")
 })
-//console.log('funciona')
 
-
-//console.log(products)
-
-//createUser('001','Gabriel','garrcastro@gmail.com','1234')
-
-//getAllUsers()
 
 
